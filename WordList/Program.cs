5cdc56a06d37583aabe758fb440fe2990b1816b7
Zesty Fernandez <superdeadman99@gmail.com>
2015-11-04 17:32:30 -0500

    Added a whitelist of allowable special characters

--- WordList/Program.cs
+++ WordList/Program.cs
@@ -17,6 +17,9 @@ namespace WordList
             var files = Directory.GetFiles(directory, "*.txt");
             Console.WriteLine("This program will read all files with the '.txt' extension in the directory of + " + directory + " which has " + files.Length + " files in it. If this is not okay please close this program immediately. Press Enter to start.");
             Console.ReadLine();
+            Console.WriteLine("This program removes any character that isn't a letter or a digit. Please list any non letter or digit you would like to appear. Please note that adding things like periods or commas may lead to unwanted results. If there is nothing you want whitelisted please just press Enter to continue.");
+            var punctuation = Console.ReadLine();
+            Console.WriteLine(punctuation);
             var count = 1;
             var word = new List<string>();
             var duplicate = new List<int>();
@@ -25,26 +28,75 @@ namespace WordList
                 string all;
                 using (var sr = new StreamReader(file))
                 {
-                    all = sr.ReadToEnd();
+                    all = sr.ReadToEnd().ToLower();
                 }
                 all = DecodeQuotedPrintables(all);
-                all = new string(all.Where(c => !char.IsPunctuation(c)).ToArray());
-                var temp = Regex.Split(all, @"\W+");
 
-                foreach (var t in temp)
+                if (string.IsNullOrEmpty(punctuation))
+                    all = new string(all.Where(c => !char.IsPunctuation(c)).ToArray());
+
+                else
                 {
+                    var sb = new StringBuilder();
+                    var counthelper = 0;
+                    foreach (var c in all)
+                    {
+                        counthelper++;
+                        if (punctuation.Contains(c))
+                        {
+                            if (punctuation.Contains("."))
+                            {
+                                if((counthelper != all.Length && counthelper < all.Length) && !string.IsNullOrEmpty(all[all.IndexOf(sb.ToString())+2].ToString()))
+                                {
+                                    sb.Append(c);
+                                }
+                                else if (punctuation.Contains("@"))
+                                {
+                                    var reverse = sb.ToString().ToArray();
+                                    Array.Reverse(reverse);
+                                    if (reverse.ToString().IndexOf("@") < reverse.ToString().IndexOf(" "))
+                                        sb.Append(c);
+                                }
+                            }
+                            else
+                                sb.Append(c);
+
+                        }
+
+                        else if (!char.IsPunctuation(c))
+                            sb.Append(c);
+
+                    }
+                    all = sb.ToString();
+                }
+                var temp = Regex.Split(all, @"\s+");
+
+                for (var i = 0; i < temp.Length; i++)
+                {
+                    temp[i] = temp[i].TrimEnd('.');
+                    if (temp[i].Contains("<"))
+                    {
+                        if (!temp[i].Contains(">"))
+                            temp[i] = temp[i].Replace("<", "");
+                    }
+                    else if (temp[i].Contains(">"))
+                    {
+                        if (!temp[i].Contains("<"))
+                            temp[i] = temp[i].Replace(">", "");
+                    }
+
                     if (word.Count == 0)
                     {
-                        word.Add(t);
+                        word.Add(temp[i]);
                         duplicate.Add(1);
                     }
-                    else if (word.Contains(t))
+                    else if (word.Contains(temp[i]))
                     {
-                        duplicate[word.IndexOf(t)]++;
+                        duplicate[word.IndexOf(temp[i])]++;
                     }
                     else
                     {
-                        word.Add(t);
+                        word.Add(temp[i]);
                         duplicate.Add(1);
                     }
                 }
